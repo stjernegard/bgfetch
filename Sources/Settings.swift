cbbb6c262d6da3b0ea1e140a1522955bc2c946b2
Danni Stjernegård <stjernegard@me.com>
2017-06-07 16:52:27 +0200

    Set output dir back to the main folder
    now that I know the script works, I can use it instead of the old Go
    script

--- Sources/Settings.swift
+++ Sources/Settings.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Settings {
     static let SourceURL = URL(string: "https://www.reddit.com/r/EarthPorn/top.json?sort=top&t=week")!
-    static let OutputDir = URL(fileURLWithPath: "/Users/dannistjernegard/Pictures/EarthPorn/swift/",
+    static let OutputDir = URL(fileURLWithPath: "/Users/dannistjernegard/Pictures/EarthPorn/",
                                isDirectory: true)
     static let NumberOfImages = 3
     static let MinDimentions = (w: 2550, h: 1430)
